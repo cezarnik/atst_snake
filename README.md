82c2de26a9a774c892c6e5541bd42bb514ca2304
Nikolay Gaivoronskiy <cezarnik2@yandex.ru>
2019-11-20 22:17:15 +0300

    change readme and add simple code to sample_script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # atst_snake
+# Testing nsnake project. 
+# The testing is partially automated using pynput library. 
+#Library installation:
+`pip3 install --user pynput`
+#To launch testing, type:
+
+`python3 sample_script.py &`. 
+
+To change the automated part, edit `sample_script.py`.
--- test/scripts/sample_script.py
+++ test/scripts/sample_script.py
@@ -1,6 +1,26 @@
 from pynput.keyboard import Key, Controller
+import time
 
-keyboard = Controller()
+def press_n(keyboard, key, n_times):
+    for i in range(n_times):
+        keyboard.press(key)
+        keyboard.release(key)
+        time.sleep(0.2)
 
-keyboard.press(Key.ctrl)
+def launch_game(keyboard):
+    keyboard.type('nsnake\n')
+    time.sleep(0.2)
 
+def play_arcade(keyboard):
+    press_n(keyboard, Key.enter, 1)
+
+def game_settings(keyboard):
+    press_n(keyboard, Key.down, 2)
+    press_n(keyboard, Key.enter, 1)
+
+if __name__ == '__main__':
+
+    keyboard = Controller()  
+    launch_game(keyboard)
+    game_settings(keyboard)
+#press_n(keyboard, Key.down, 1)
